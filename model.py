c322903732c4eab68c39dc8abb8d91bfa3d9cfa9
BuildTools <chrisconcord@gmail.com>
2021-02-14 04:17:06 -0500

    fixed printing loss values while training

--- model.py
+++ model.py
@@ -77,9 +77,9 @@ for epoch in range(2):
         optimizer.step()
         # print statistics
         running_loss += loss.item()
-        if i & 2000 == 1999:
+        if i % 100 == 99:
             print('[%d, %5d] loss: %.3f' %
-                (epoch + 1, i + 1, running_loss / 2000))
+                (epoch + 1, i + 1, running_loss / 100))
             running_loss = 0.0
 print('Done training')
 PATH = "./image_net.pth"
